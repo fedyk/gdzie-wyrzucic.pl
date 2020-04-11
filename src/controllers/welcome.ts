c7e44e3a4a924d5bb09129920b368feaecaa2837
Andrew <fedyk72@gmail.com>
2020-04-11 21:26:30 +0200

    Bump dependencies

--- src/controllers/welcome.ts
+++ src/controllers/welcome.ts
@@ -1,6 +1,5 @@
 import { Middleware } from "koa";
 import { welcomeView } from "../views/welcome-view";
-import { format } from "util";
 import { AppState, AppContext } from "../types";
 
 export const welcome: Middleware<AppState, AppContext> = function(ctx) {
