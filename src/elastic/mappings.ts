a866c04d1a1f7a7748b406869a331d23947477cb
Andrew <fedyk72@gmail.com>
2020-04-12 19:04:14 +0200

    Add link for more info

--- src/elastic/mappings.ts
+++ src/elastic/mappings.ts
@@ -2,6 +2,8 @@ import { Client } from "@elastic/elasticsearch"
 import { config } from "../config"
 import { WASTES_INDEX, CATEGORIES_INDEX } from "./constants"
 
+/** @see https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping.html */
+
 const client = new Client({
   node: config.ELASTICSEARCH_URL
 })
